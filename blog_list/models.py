468dcac270f3ff66400377ad1fb15351fdd5f682
alexHooper <alexmohack@gmail.com>
2018-08-08 19:22:42 +0530

    create model Blog for our blog_list

--- blog_list/models.py
+++ blog_list/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+	title = models.CharField(max_length=50, help_text="Title for our blog", unique_for_date="published_date")
+	content = models.TextField(help_text="Content for our blog")
+	created_date = models.DateTimeField(auto_now_add=True, help_text="Date on which the blog is made")
+	published_date = models.DateField(help_text="Date on which the blog will be published", verbose_name="Published Date")
+	edited_date = models.DateField(auto_now=True)
