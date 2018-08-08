7f772124dc60f267053bb259483c29e123477807
alexHooper <alexmohack@gmail.com>
2018-08-08 19:40:36 +0530

    add str method in our blog

--- blog_list/models.py
+++ blog_list/models.py
@@ -6,5 +6,8 @@ class Blog(models.Model):
 	title = models.CharField(max_length=50, help_text="Title for our blog", unique_for_date="published_date")
 	content = models.TextField(help_text="Content for our blog")
 	created_date = models.DateTimeField(auto_now_add=True, help_text="Date on which the blog is made")
-	published_date = models.DateField(help_text="Date on which the blog will be published", verbose_name="Published Date", default=datetime.now() + timedelta(days=5))
+	published_date = models.DateField(help_text="Default blog publish date of 5 days later", verbose_name="Published Date", default=datetime.now() + timedelta(days=5))
 	edited_date = models.DateField(auto_now=True)
+
+	def __str__(self):
+		return f"Blog: {self.title}"
