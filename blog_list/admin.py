bb3f69a575d828e3b74d42a224524fa6b7235d44
alexHooper <alexmohack@gmail.com>
2018-08-08 19:23:00 +0530

    register our model Blog in admin site

--- blog_list/admin.py
+++ blog_list/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Blog
+
+@admin.register(Blog)
+class BlogModelAdmin(admin.ModelAdmin):
+	list_display = ('title', 'created_date', 'published_date', 'edited_date')
+	search_fields = ('title', 'content')
