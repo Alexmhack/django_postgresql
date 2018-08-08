d6e8d5fa813f77fd40a859f93b32df6492d02133
alexHooper <alexmohack@gmail.com>
2018-08-08 19:32:59 +0530

    add default date of 5 days later in our published_date

--- blog_list/models.py
+++ blog_list/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+from datetime import datetime, timedelta
+
 class Blog(models.Model):
 	title = models.CharField(max_length=50, help_text="Title for our blog", unique_for_date="published_date")
 	content = models.TextField(help_text="Content for our blog")
 	created_date = models.DateTimeField(auto_now_add=True, help_text="Date on which the blog is made")
-	published_date = models.DateField(help_text="Date on which the blog will be published", verbose_name="Published Date")
+	published_date = models.DateField(help_text="Date on which the blog will be published", verbose_name="Published Date", default=datetime.now() + timedelta(days=5))
 	edited_date = models.DateField(auto_now=True)
