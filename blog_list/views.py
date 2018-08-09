6fcd54b97c608abb620f2b87a7df86b1a2de3b41
alexHooper <alexmohack@gmail.com>
2018-08-09 13:46:28 +0530

    change the logic for getting latest posts from last three days

--- blog_list/views.py
+++ blog_list/views.py
@@ -12,7 +12,7 @@ class BlogListView(generic.ListView):
 	def get_context_data(self, **kwargs):
 		context = super(BlogListView, self).get_context_data(**kwargs)
 		context['latest_list'] = Blog.objects.filter(
-			published_date__lte=datetime.now() + timedelta(days=-3))[:5]
+			published_date__gte=datetime.now() - timedelta(days=3))[:5]
 		return context
 
 
