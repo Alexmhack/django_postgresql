a4e4f770995a4ec015f167f77eba11ba5747eb83
alexHooper <alexmohack@gmail.com>
2018-08-09 13:46:55 +0530

    add urls for login logout

--- blog/urls.py
+++ blog/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_list.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
