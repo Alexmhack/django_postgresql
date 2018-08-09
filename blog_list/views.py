5ada326c66f7aa66282e0d59a5e8d0776a519c1e
alexHooper <alexmohack@gmail.com>
2018-08-09 13:27:49 +0530

    add context for latest blogs

--- blog_list/views.py
+++ blog_list/views.py
@@ -3,8 +3,17 @@ from django.views import generic
 
 from .models import Blog
 
+from datetime import datetime
+from datetime import timedelta
+
 class BlogListView(generic.ListView):
 	model = Blog
+
+	def get_context_data(self, **kwargs):
+		context = super(BlogListView, self).get_context_data(**kwargs)
+		context['flight_list_view'] = Blog.objects.filter(
+			published_date__lte=datetime.now() + timedelta(days=-3))[:5]
+		return context
 
 
 class BlogDetailView(generic.DetailView):
