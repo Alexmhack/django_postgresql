539d4891e2e657bdbdc3fb5185407906c6cf0acf
alexHooper <alexmohack@gmail.com>
2018-08-09 11:43:35 +0530

    add url for blog detail

--- blog_list/urls.py
+++ blog_list/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 
 from .views import (
 	BlogListView,
+	BlogDetailView,
 )
 
 urlpatterns = [
 	path('', BlogListView.as_view(), name='blog-list'),
+	path('blog/<int:pk>/detail', BlogDetailView.as_view(), name='blog-detail'),
 ]
